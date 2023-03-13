c9034fbdd6dbff850191ca3e82316fa1dd7faa23
guzhexuan <2974127659@qq.com>
2023-03-14 00:11:30 +0800

    output shape correct

--- Task2/mlpAttentionTest.py
+++ Task2/mlpAttentionTest.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 import torch
 import torch.nn as nn
+from model import DenseNet
 
+net = DenseNet(100, 64, 64, 10)
 
-embed = nn.Embedding(10, 100)
-x = torch.arange(10)
-
-output = embed(x)
+cities = torch.randint(9, size=(10, 200))
+inputx = torch.randn((10, 200, 100))
+output = net(inputx, cities)
 print(output.shape)
