29f553ddc4b10455f75ba4a52d19b692d9e64f92
AUYEUNG99 <1443707149@qq.com>
2023-03-14 22:22:07 +0800

    heatmap

---
+++ Task2/figure/figure.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+
+
+def readData(fileName):
+    df = pd.read_csv(os.path.dirname(
+        __file__) + fileName, encoding='unicode_escape')
+    return df
+
+
+df = readData("/train_attrs.csv")
+
+# print(df)
+sns.set_context({"figure.figsize": (8, 8)})
+sns.heatmap(df.corr(), annot=True, vmax=1, square=True,
+            cmap="Blues", annot_kws={"fontsize": 7})
+plt.show()
