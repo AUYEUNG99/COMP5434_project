59116828371b29bc10707a64685a8c9d9f410a9a
AUYEUNG99 <1443707149@qq.com>
2023-03-17 13:00:03 +0800

    try2

--- Task2/ML/sol.py
+++ Task2/ML/sol.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import os
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import Normalizer, MaxAbsScaler
 from sklearn import preprocessing
 from sklearn.model_selection import StratifiedKFold, train_test_split
 from catboost import CatBoostClassifier
@@ -41,16 +41,16 @@ if __name__ == "__main__":
     trainData = countTotalCost(trainData)
     for totalCost, index in zip(trainData['total cost'], trainData.index):
         trainData.loc[index, 'price range'] = classify(totalCost)
-
-    oneHotColumns = ['city', 'zip code']
-    dum = pd.get_dummies(trainData,
-                         columns=oneHotColumns, drop_first=True)
     # print(dum.info())
     trainData['date'] = pd.to_datetime(trainData['date'])
     trainData['year'] = trainData['date'].dt.year
     trainData['month'] = trainData['date'].dt.month
     trainData['day'] = trainData['date'].dt.day
-    dropColumns = ['district', 'region', 'date',
+    oneHotColumns = ['city', 'zip code', 'year', 'month',
+                     'day', 'building year']
+    dum = pd.get_dummies(trainData,
+                         columns=oneHotColumns, drop_first=True)
+    dropColumns = ['district', 'region', 'date', 'decoration year',
                    'unit price of residence space', 'unit price of building space', 'total cost']
     dum = dum.drop(
         dropColumns, axis=1)
@@ -59,46 +59,42 @@ if __name__ == "__main__":
     trainData = trainData.drop(
         dropColumns, axis=1)
 
-    # trainData = (trainData - trainData.min()) / \
-    #     (trainData.max() - trainData.min())
-
     trainData = trainData.merge(dum, how="left")
-    trainData[['number of rooms', 'security level of the community', 'residence space', 'building space', 'noise level',
-               'view', 'air quality level', 'aboveground space ', 'basement space', 'exchange rate']].apply(max_min_scaler)
+    # trainData[['number of rooms', 'security level of the community', 'residence space', 'building space', 'noise level',
+    #            'view', 'air quality level', 'aboveground space ', 'basement space', 'exchange rate']].apply(max_min_scaler)
     trainData.to_csv(os.path.dirname(
         __file__)+"/train_data.csv")
     trainData['price range'] = trainData['price range'].astype('category')
 
     dataX = trainData.drop(['price range'], axis=1)
+    # preprocess = MinMaxScaler()
+    # dataX = preprocess.fit_transform(dataX)
+    # # preprocess = MaxAbsScaler()
+    # # dataX = preprocess.fit_transform(dataX)
     dataY = trainData['price range']
 
     # trainX, testX, trainY, testY = train_test_split(
-    #     dataX, dataY, test_size=0.3)
-    # kf = KFold(n_splits=5, random_state=42, shuffle=True)
-    # for i, (trainIndex, testIndex) in enumerate(kf.split(dataX, dataY)):
-    #     trainX, testX = dataX.iloc[trainIndex,
-    #                                :], dataX.iloc[testIndex, :]
-    #     trainY, testY = dataY[trainIndex], dataY[testIndex]
-
-    #     model = CatBoostClassifier(iterations=5000,
-    #                                depth=5,
-    #                                learning_rate=0.05,
-    #                                loss_function='MultiClass',
-    #                                logging_level='Verbose')
-    #     model.fit(trainX, trainY)
-    #     pred = model.predict(testX)
-    #     acc = accuracy_score(testY, pred)
-    #     print(acc)
-    #     pred = model.predict(trainX)
-    #     acc = accuracy_score(trainY, pred)
-    #     print(acc)
-
+    #     dataX, dataY, test_size=0.1, shuffle=True)
+    # model = CatBoostClassifier(iterations=10000,
+    #                            depth=5,
+    #                            learning_rate=0.01,
+    #                            loss_function='MultiClass',
+    #                            logging_level='Verbose',
+    #                            random_seed=2023)
+    # model.fit(trainX, trainY)
+    # pred = model.predict(testX)
+    # acc = accuracy_score(testY, pred)
+    # print(acc)
+    # pred = model.predict(trainX)
+    # acc = accuracy_score(trainY, pred)
+    # print(acc)
+    # print(trainData.info())
     strtfdKFold = StratifiedKFold(n_splits=10)
     kfold = strtfdKFold.split(dataX, dataY)
     scores = []
     for k, (train, test) in enumerate(kfold):
-        model = CatBoostClassifier(iterations=5000,
-                                   depth=6,
+        model = CatBoostClassifier(iterations=10000,
+                                   depth=5,
                                    learning_rate=0.01,
                                    loss_function='MultiClass',
                                    logging_level='Silent',
