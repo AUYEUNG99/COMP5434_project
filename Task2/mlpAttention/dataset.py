c4b48dfe1e0db1b9234fd1f51d70e3e6b2731119
guzhexuan <2974127659@qq.com>
2023-03-20 14:39:28 +0800

    create dataset class for validation

--- Task2/mlpAttention/dataset.py
+++ Task2/mlpAttention/dataset.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 from torch.utils.data import Dataset
 
 
@@ -8,14 +7,45 @@ class HousepriceDataset(Dataset):
         super(HousepriceDataset, self).__init__()
         self.data = data
         self.city = city
-        self.label = label - 1
+        self.label = label
         self.num = train_num
 
     def __len__(self):
         return len(self.label) - self.num
 
     def __getitem__(self, index):
+        """
+        data = torch.Tensor(self.data[index])
+        city = torch.tensor(self.city[index], dtype=torch.int)
+        label = torch.tensor(self.label[index], dtype=torch.long)
+        """
+
         data = torch.Tensor(self.data[index:index + self.num])
         city = torch.tensor(self.city[index:index + self.num], dtype=torch.int)
         label = torch.tensor(self.label[index:index + self.num], dtype=torch.long)
+
+        return data, city, label
+
+class ValidationDataset(Dataset):
+    def __init__(self, data, city, label, train_num):
+        super(ValidationDataset, self).__init__()
+        self.data = data
+        self.city = city
+        self.label = label
+        self.num = train_num
+
+    def __len__(self):
+        return len(self.label) // self.num
+
+    def __getitem__(self, index):
+        """
+        data = torch.Tensor(self.data[index])
+        city = torch.tensor(self.city[index], dtype=torch.int)
+        label = torch.tensor(self.label[index], dtype=torch.long)
+        """
+
+        data = torch.Tensor(self.data[index * self.num:index * self.num + self.num])
+        city = torch.tensor(self.city[index * self.num:index * self.num + self.num], dtype=torch.int)
+        label = torch.tensor(self.label[index * self.num:index * self.num + self.num], dtype=torch.long)
+
         return data, city, label
