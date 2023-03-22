b6cbe1edde5ff79a549a59417da17ea80b0e6be9
guzhexuan <2974127659@qq.com>
2023-03-22 16:16:24 +0800

    original dataset + cities, zipcodes embedding

--- Task2/mlpAttention/mlpAttentionTest.py
+++ Task2/mlpAttention/mlpAttentionTest.py
@@ -12,27 +12,34 @@ test = DataProcess("../../Task1/train_data_final", unused_attrs=['district', 'ci
                                                                  'unit price of residence space',
                                                                  'unit price of building space', 'total cost', ])
 
-l1, l2, l3 = test.getdata(normalize=True)
+l1, l2, l3, l4 = test.getdata(normalize=True)
+"""
+l1: data
+l2: citys
+l3: zipcodes
+l4: label
+"""
+
 train_num = int(len(l1) * 0.9)
 city_num = len(np.unique(l2))
+zip_num = len(np.unique(l3))
 """
 use augmented dataset to replace original dataset
 """
-l1 = pd.read_csv("../ML/train_data18.csv").to_numpy()
-l1 = l1[:, :-4]
-l1 = preprocessing.normalize(l1, axis=0)
+
 train_x, valid_x = l1[:train_num], l1[train_num:]
 train_city, valid_city = l2[:train_num], l2[train_num:]
-train_label, valid_label = l3[:train_num], l3[train_num:]
+train_zipcodes, valid_zipcodes = l3[:train_num], l3[train_num:]
+train_label, valid_label = l4[:train_num], l4[train_num:]
 
-train_dataset = HousepriceDataset_V2(train_x, train_city, train_label, 40)
-test_dataset = HousepriceDataset_V2(valid_x, valid_city, valid_label, 20)
+train_dataset = HousepriceDataset_V2(train_x, train_city, train_label, train_zipcodes, 40)
+test_dataset = HousepriceDataset_V2(valid_x, valid_city, valid_label, valid_zipcodes, 20)
 train_dataloader = DataLoader(train_dataset, batch_size=20, shuffle=True,
                               )
 
 test_dataloader = DataLoader(test_dataset, batch_size=10, shuffle=True,
                              )
-model = DenseNet(l1.shape[1], 8, 32, city_num)
+model = DenseNet(l1.shape[1], 8, 32, city_num, zip_num)
 
 trl, tra, tel, tea = list(), list(), list(), list()
 train(train_dataloader, model, test_dataloader, trl, tra, tel, tea)
