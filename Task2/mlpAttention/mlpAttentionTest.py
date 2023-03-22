08d379c2aab6b9a257a7bc41a092c840bb092f53
guzhexuan <2974127659@qq.com>
2023-03-22 14:31:48 +0800

    only use city embedding and use data augmentation dataset

--- Task2/mlpAttention/mlpAttentionTest.py
+++ Task2/mlpAttention/mlpAttentionTest.py
@@ -4,46 +4,35 @@ import numpy as np
 from preprocess import DataProcess
 from mlp_global_attn import DenseNet
 from train import train
+from sklearn import preprocessing
+from torch.utils.data import DataLoader
+import pandas as pd
 
-test = DataProcess("./train_data_final", unused_attrs=['district', 'city', 'zip code', 'region',
-                                                       'unit price of residence space',
-                                                       'exchange rate',
-                                                       'unit price of building space', 'total cost',
+test = DataProcess("../../Task1/train_data_final", unused_attrs=['district', 'city', 'zip code', 'region',
+                                                                 'unit price of residence space',
+                                                                 'unit price of building space', 'total cost', ])
 
-                                                       ])
-
-l1, l2, l3, l4 = test.getdata(normalize=True)
+l1, l2, l3 = test.getdata(normalize=True)
 train_num = int(len(l1) * 0.9)
 city_num = len(np.unique(l2))
-zip_num = len(np.unique(l3))
+"""
+use augmented dataset to replace original dataset
+"""
+l1 = pd.read_csv("../ML/train_data18.csv").to_numpy()
+l1 = l1[:, :-4]
+l1 = preprocessing.normalize(l1, axis=0)
 train_x, valid_x = l1[:train_num], l1[train_num:]
 train_city, valid_city = l2[:train_num], l2[train_num:]
-train_zips, valid_zips = l3[:train_num], l3[train_num:]
-train_label, valid_label = l4[:train_num], l4[train_num:]
+train_label, valid_label = l3[:train_num], l3[train_num:]
 
-"""
-因为现在的数据集文件 total cost没补充, 值是nan，所以label均为4！
-"""
-
-"""
-net = DenseNet(100, 64, 64, 10)
-
-cities = torch.randint(9, size=(10, 200))
-inputx = torch.randn((10, 200, 100))
-output = net(inputx, cities)
-print(output.shape)
-"""
-
-train_dataset = HousepriceDataset_V2(train_x, train_city, train_label, train_zips, 40)
-test_dataset = HousepriceDataset_V2(valid_x, valid_city, valid_label, valid_zips, 20)
-print(len(train_dataset))
-print(len(test_dataset))
+train_dataset = HousepriceDataset_V2(train_x, train_city, train_label, 40)
+test_dataset = HousepriceDataset_V2(valid_x, valid_city, valid_label, 20)
 train_dataloader = DataLoader(train_dataset, batch_size=20, shuffle=True,
                               )
 
 test_dataloader = DataLoader(test_dataset, batch_size=10, shuffle=True,
                              )
-model = DenseNet(l1.shape[1], 8, 32, city_num, zip_num)
+model = DenseNet(l1.shape[1], 8, 32, city_num)
 
-train(train_dataloader, model, test_dataloader)
-
+trl, tra, tel, tea = list(), list(), list(), list()
+train(train_dataloader, model, test_dataloader, trl, tra, tel, tea)
