da3c0ee3f8c05ad1df986489880f2fd58f1231d1
AUYEUNG99 <1443707149@qq.com>
2023-04-12 21:50:24 +0800

    final code

--- Task2/ML/sol.py
+++ Task2/ML/sol.py
@@ -7,6 +7,8 @@ from sklearn import preprocessing
 from sklearn.model_selection import StratifiedKFold, train_test_split
 from catboost import CatBoostClassifier
 import datetime as dt
+import optuna
+from optuna.samplers import TPESampler
 
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
 
@@ -37,20 +39,27 @@ def classify(totalCost):
         return 4
 
 
-# def process(df, item, top):
-#     list_of_items = list(df[item].apply(
-#         lambda x: [i['name'] for i in x] if x != {} else []).values)
-#     df.loc[:, 'num_' + item] = df[item].apply(
-#         lambda x: len(x) if x != {} else 0)
-#     df.loc[:, 'all_' + item] = df[item].apply(
-#         lambda x: ' '.join(sorted([i['name'] for i in x])) if x != {} else '')
-#     top_items = [m[0] for m in Counter(
-#         [i for j in list_of_items for i in j]).most_common(top)]
-#     for g in top_items:
-#         df.loc[:, item + '_' +
-#                g] = df.loc[:, 'all_' + item].apply(lambda x: 1 if g in x else 0)
-#     df = df.drop(['all_' + item], axis=1)
-#     return df
+def objective(trial):
+    X_train, X_test, y_train, y_test = train_test_split(
+        dataX, dataY, test_size=0.1)
+    param = {
+        "iterations": trial.suggest_int("iterations", 100, 5000),
+        "loss_function": trial.suggest_categorical("loss_function", ["MultiClass"]),
+        "learning_rate": trial.suggest_float("learning_rate", 1e-5, 1e0),
+        "l2_leaf_reg": trial.suggest_float("l2_leaf_reg", 1e-2, 10),
+        "colsample_bylevel": trial.suggest_float("colsample_bylevel", 0.01, 1),
+        "bagging_temperature": trial.suggest_float("bagging_temperature", 1e-2, 1e0),
+        "depth": trial.suggest_int("depth", 5, 10),
+        "min_data_in_leaf": trial.suggest_int("min_data_in_leaf", 2, 20),
+    }
+
+    reg = CatBoostClassifier(
+        **param, cat_features=categorical_features_indices)
+    reg.fit(X_train, y_train, eval_set=[
+            (X_test, y_test)], verbose=0, early_stopping_rounds=100)
+    y_pred = reg.predict(X_test)
+    score = accuracy_score(y_test, y_pred)
+    return score
 
 
 if __name__ == "__main__":
@@ -58,7 +67,6 @@ if __name__ == "__main__":
     trainData = countTotalCost(trainData)
     for totalCost, index in zip(trainData['total cost'], trainData.index):
         trainData.loc[index, 'price range'] = classify(totalCost)
-    # print(dum.info())
     trainData['date'] = pd.to_datetime(trainData['date'])
     trainData['year'] = trainData['date'].dt.year
     trainData['month'] = trainData['date'].dt.month
@@ -71,84 +79,57 @@ if __name__ == "__main__":
                   'decoration year'] = trainData['building year']
     trainData.loc[trainData['decoration year'] < trainData['building year'],
                   'decoration year'] = trainData['building year']
-    trainData['house_age'] = trainData['year'] - trainData['building year']
-    trainData['decorate_age'] = trainData['year'] - \
-        trainData['decoration year']
-    trainData['space_ratio'] = trainData['aboveground space '] / \
-        trainData['residence space']
-    trainData['room_space'] = trainData['residence space'] / \
-        (trainData['number of rooms'] + 1)
-    # trainData['second_sell'] = 0
-    # trainData.loc[trainData.duplicated(
-    #     ['district']) == True, 'second_sell'] = 1
-    trainData['median_rank_zip code'] = trainData.groupby(
-        'zip code')['price range'].transform('median')
-    trainData['median_rank_city'] = trainData.groupby(
-        'city')['price range'].transform('median')
-    trainData['mean_rank_zip code'] = trainData.groupby(
-        'zip code')['price range'].transform('mean')
-    trainData['mean_rank_city'] = trainData.groupby(
-        'city')['price range'].transform('mean')
-    oneHotColumns = []
-    dum = pd.get_dummies(trainData,
-                         columns=oneHotColumns, drop_first=True)
-    dropColumns = ['waterfront', 'zip code', 'city', 'district',  'region', 'year', 'month', 'day', 'date', 'decoration year',
-                   'basement space', 'aboveground space ', 'building year', 'unit price of residence space', 'unit price of building space', 'total cost']
-    dum = dum.drop(
-        dropColumns, axis=1)
-    trainData = trainData.drop(
-        oneHotColumns, axis=1)
+    # trainData['house_age'] = trainData['year'] - trainData['building year']
+    # trainData['decoration_age'] = trainData['year'] - \
+    #     trainData['decoration year']
+    # trainData['median_rank_zip code'] = trainData.groupby(
+    #     'zip code')['price range'].transform('median')
+    # trainData['median_rank_city'] = trainData.groupby(
+    #     'city')['price range'].transform('median')
+    # trainData['mean_rank_zip code'] = trainData.groupby(
+    #     'zip code')['price range'].transform('mean')
+    # trainData['mean_rank_city'] = trainData.groupby(
+    #     'city')['price range'].transform('mean')
+    # oneHotColumns = []
+    # dum = pd.get_dummies(trainData,
+    #                      columns=oneHotColumns, drop_first=True)
+    dropColumns = ['district',  'region', 'year', 'month', 'day', 'date',
+                    'unit price of residence space', 'unit price of building space', 'total cost']
+
+    # dum = dum.drop(
+    #     dropColumns, axis=1)
+    # trainData = trainData.drop(
+    #     oneHotColumns, axis=1)
     trainData = trainData.drop(
         dropColumns, axis=1)
-
-    trainData = trainData.merge(dum, how="left")
-
-    # trainData = trainData.drop(trainData.index[0], axis=1)
-    # trainData[['number of rooms', 'security level of the community', 'residence space', 'building space', 'noise level',
-    #            'view', 'air quality level', 'aboveground space ', 'basement space', 'exchange rate']].apply(max_min_scaler)
+    # trainData = trainData.merge(dum, how="left")
+    print(trainData.info())
+    trainData[['number of rooms', 'security level of the community', 'residence space', 'building space', 'noise level', 'waterfront',
+               'view', 'air quality level', 'aboveground space ', 'basement space', 'exchange rate',]].apply(max_min_scaler)
     trainData.to_csv(os.path.dirname(
         __file__)+"/train_data.csv", index=False)
+
     # trainData['price range'] = trainData['price range'].astype('category')
 
     dataX = trainData.drop(['price range'], axis=1)
-    preprocess = MinMaxScaler()
-    dataX = preprocess.fit_transform(dataX)
-    # preprocess = MaxAbsScaler()
-    # dataX = preprocess.fit_transform(dataX)
+
     dataY = trainData['price range']
+    categorical_features_indices = np.where(dataX.dtypes != np.float64)[0]
+    study = optuna.create_study(sampler=TPESampler(), direction="maximize")
+    study.optimize(objective, n_trials=30, timeout=60)
 
-    trainX, testX, trainY, testY = train_test_split(
-        dataX, dataY, test_size=0.3, shuffle=True)
-    model = CatBoostClassifier(iterations=5000,
-                               depth=7,
-                               learning_rate=0.01,
-                               loss_function='MultiClass',
-                               logging_level='Verbose',
-                               random_seed=2023)
-    model.fit(trainX, trainY)
-    pred = model.predict(testX)
-    acc = accuracy_score(testY, pred)
-    print(acc)
-    pred = model.predict(trainX)
-    acc = accuracy_score(trainY, pred)
-    print(acc)
-    print(trainData.info())
-    # strtfdKFold = StratifiedKFold(n_splits=10)
-    # kfold = strtfdKFold.split(dataX, dataY)
-    # scores = []
-    # for k, (train, test) in enumerate(kfold):
-    #     model = CatBoostClassifier(iterations=10000,
-    #                                depth=5,
-    #                                learning_rate=0.01,
-    #                                loss_function='MultiClass',
-    #                                logging_level='Silent',
-    #                                random_seed=2023)
 
-    #     model.fit(dataX.iloc[train, :], dataY.iloc[train])
-    #     trainScore = model.score(dataX.iloc[train, :], dataY.iloc[train])
-    #     testScore = model.score(dataX.iloc[test, :], dataY.iloc[test])
-    #     scores.append(testScore)
-    #     print('Fold: %2d, Training accuracy: %.3f, Testing accuracy: %.3f' %
-    #           (k+1, trainScore, testScore))
-    # print('Cross-Validation accuracy: %.3f +/- %.3f' %
-    #       (np.mean(scores), np.std(scores)))
+    strtfdKFold = StratifiedKFold(n_splits=10)
+    kfold = strtfdKFold.split(dataX, dataY)
+    scores = []
+    for k, (train, test) in enumerate(kfold):
+        model = CatBoostClassifier(
+            **study.best_params, cat_features=categorical_features_indices, silent=True)
+        model.fit(dataX.iloc[train, :], dataY.iloc[train])
+        trainScore = model.score(dataX.iloc[train, :], dataY.iloc[train])
+        testScore = model.score(dataX.iloc[test, :], dataY.iloc[test])
+        scores.append(testScore)
+        print('Fold: %2d, Training accuracy: %.3f, Testing accuracy: %.3f' %
+              (k+1, trainScore, testScore))
+    print('Cross-Validation accuracy: %.3f +/- %.3f' %
+          (np.mean(scores), np.std(scores)))
